dd8e727a8f240112d0d075d2f695335c0e741c14
Nathan Potter <ndp@opendap.org>
2015-01-09 06:33:30 -0800

    Adding libraries reqiured for compile but not part of the distribution...

--- ncwms-build.sh
+++ ncwms-build.sh
@@ -1,1 +1,1 @@
-ant -Dj2ee.platform.classpath=/usr/share/tomcat7/lib/servlet-api.jar -Dlibs.CopyLibs.classpath=/home/guy/Code/copylibstask.jari $@
+ant -Dj2ee.platform.classpath=lib/servlet-api-2.5.jar -Dlibs.CopyLibs.classpath=lib/copylibstask.jar $@
